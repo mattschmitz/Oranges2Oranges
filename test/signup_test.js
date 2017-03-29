edfc42d83a5b6e8c90e229d35c67fd25b9f585c8
justinjyoo@gmail.com <justinyoo@Justins-MacBook-Pro.local>
2017-03-29 12:25:32 -0700

    Add 3 more tests for empty username/password fields and redirect /lobby

--- test/signup_test.js
+++ test/signup_test.js
@@ -9,19 +9,16 @@ var dbIndex = require('../db/index.js');
 var db = dbIndex.db
 var userModel = dbIndex.userModel
 
-describe('testroute1', function() {
-  it('should accept signup', function(done) {
-    server
-    .get('/testroute1')
-    .expect(200)
-    .end(done);
-  });
-});
-
 var userOne = {
 	username: 'justinisgreat', 
 	email: 'justinisgreat@gmail.com', 
 	password: 'justinisgreat'
+}
+
+var userTwo = {
+	username: 'userLobby', 
+	email: 'userLobby@gmail.com', 
+	password: 'userLobby'
 }
 
 after(function(done) {
@@ -60,6 +57,47 @@ describe('user sign up', function() {
 			done();
 		})
   })
+
+  it('should respond with an error status of 400 when username field is empty', function (done) {
+  	server
+  	.post('/signup')
+  	.send({
+			username: '', 
+			email: 'justinisjustokay@gmail.com', 
+			password: 'justinisjustokay'
+		})
+		.expect(400)
+		.end(function(err, res) {
+			if(err) return done(err);
+			done();
+		})
+  })
+
+  it('should respond with an error status of 400 when password field is empty', function (done) {
+  	server
+  	.post('/signup')
+  	.send({
+			username: 'justinisaverage', 
+			email: 'justinisaverage@gmail.com', 
+			password: ''
+		})
+		.expect(400)
+		.end(function(err, res) {
+			if(err) return done(err);
+			done();
+		})
+  })
+
+  it('should redirect to /lobby', function(done) {
+  	server
+  	.post('/signup')
+  	.send(userTwo)
+  	.expect('Location', '/lobby')
+  	.end(function(err, res) {
+  		done()
+  	})
+  })
+
 })
 
 
