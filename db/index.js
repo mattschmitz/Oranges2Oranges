488998f1a004cfaf6f625902e6638f2e1e73adfc
Tyler Walker <tyjohnwalker@gmail.com>
2017-03-29 19:31:59 -0700

    delete logs

--- db/index.js
+++ db/index.js
@@ -14,9 +14,6 @@ var dummyGamePlayThrough = require('./game-playthrough-dummy-data.js')
 
 var db = mongoose.connection;
 
-console.log('here we are in index.js, mongodbURL= ', mongodbURL);
-console.log('process.env =',  process.env);
-process.stdout.write('the mongodbURL =' + mongodbURL);
 db.on('error', function() {
   console.log('mongoose connection error');
 });
