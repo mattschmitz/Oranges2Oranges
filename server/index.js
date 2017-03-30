df88ce6a8229a1ab3ddd27519aebefdc33b0b1e8
Tyler Walker <tyjohnwalker@gmail.com>
2017-03-30 14:18:12 -0700

    change to array prototype includes

--- server/index.js
+++ server/index.js
@@ -126,8 +126,9 @@ io.on('connection', (socket) => {
     queries.retrieveGameInstance(gameName)
     .then(function (game){  
     // add client to game DB if they're not already in players list
+    //!game.players.includes(username)
     console.log('in server, join game, game:', game);
-      if (!game.players.includes(username)) {
+      if (!(Array.prototype.includes(game, username))) {
         var players = game.players.slice(0);
         players.push(username);
         return queries.addPlayerToGameInstance(gameName, players);
