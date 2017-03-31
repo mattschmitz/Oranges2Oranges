039a862b5e53a49b65f4550de3982b9e0e2fe5f6
Innie Kim <innie.jong.kim@gmail.com>
2017-03-31 11:16:40 -0700

    Host can start game with any number of players - TODO: Add the ability for other players to signal they are ready - TODO: Add the ability to set the number of rounds
    Moved Start Game button to underneath player list

--- client/src/components/WaitingRoom.jsx
+++ client/src/components/WaitingRoom.jsx
@@ -1,24 +1,27 @@
 'use strict';
 import React from 'react';
 import Rules from './Rules.jsx';
-import { Col, PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Col, PageHeader, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
-const WaitingRoom = (props) => (
-  <Col id='waiting-room'>
-  <PageHeader>{props.game.gameName} <small>Waiting Room</small></PageHeader>
-    <h3>Number of Players: {props.game.players.length} / 4</h3>
-    <br />
-    <h4>Current Players:</h4>
-    <Col sm={4} smOffset={4}>
-      <ListGroup>
-        {props.game.players.map( (player) => <ListGroupItem>{player}</ListGroupItem>)}
-      </ListGroup>
+const WaitingRoom = (props) => {
+  return (
+    <Col id='waiting-room'>
+      <PageHeader>{props.game.gameName} <small>Waiting Room</small></PageHeader>
+      <h3>Number of Players: {props.game.players.length}</h3>
+      <br />
+      <h4>Current Players:</h4>
+      <Col sm={4} smOffset={4}>
+        <ListGroup>
+          {props.game.players.map( (player) => <ListGroupItem>{player}</ListGroupItem>)}
+        </ListGroup>
+      </Col>
+      <Col sm={6} smOffset={3}>
+        {props.game.host === props.user && <Button onClick={props.startgame}>Start Game!</Button>}
+        <Rules/>
+      </Col>
     </Col>
-    <Col sm={6} smOffset={3}>
-      <Rules/>
-    </Col>
-  </Col>
-)
+  )
+}
 
 export default WaitingRoom;
 
